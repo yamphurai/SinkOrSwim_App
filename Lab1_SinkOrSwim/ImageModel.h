c42d69bb8ad0eea1351f7699b2ee8884f1434d43
Jeevan Rai <yamphurai007@yahoo.com>
2024-09-12 12:37:10 -0500

    Upto tableViewController class

---
+++ Lab1_SinkOrSwim/ImageModel.h
@@ -0,0 +1,16 @@
+//
+//  ImageModel.h
+//  Lab1_SinkOrSwim
+//
+//  Created by Reshma Shrestha on 9/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lab1_SinkOrSwim/ImageModel.m
@@ -0,0 +1,12 @@
+//
+//  ImageModel.m
+//  Lab1_SinkOrSwim
+//
+//  Created by Reshma Shrestha on 9/12/24.
+//
+
+#import "ImageModel.h"
+
+@implementation ImageModel
+
+@end
